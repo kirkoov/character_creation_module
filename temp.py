621e49c6d03ebc04c3f3893839e9841eca9385e5
Кирилл Кувшинов <k.koovshinov@gmail.com>
2023-01-20 23:02:59 +0300

    Add more trials to check locally as under PEP8, etc.

--- temp.py
+++ temp.py
@@ -1,9 +1,55 @@
-# def we_crash_all(name: str) -> str:
-#     return 'Привет, ' + name + ', мы всё сломали!'
+"""Документация модуля. Описывает работу классов и функций.
+Размещается в верхней части файла (начиная с первой строки).
+"""
+# # Импортируйте модуль inspect.
+# import inspect
 
 
-# print(we_crash_all('Suzan'))
+# def tricky_func(self):
+#     """Описывает работу функции tricky_func."""
+#     ...
 
+
+# class Test:
+#     """Класс Test используется для демонстрации docstring."""
+
+#     def first(self) -> str:
+#         """Описывает метод first и демонстрирует перенос строки
+#         документации.
+#         """
+#         return ''
+
+
+# print('Без применения cleandoc:')
+# print(Test.first.__doc__)
+# print('С применением cleandoc:')
+# # Выведите докстринг, используя метод cleandoc().
+# print(inspect.cleandoc(Test.first.__doc__))
+
+from math import sqrt
+
+
+def calculate_square_root(number: float) -> float:
+    """Вычисляет квадратный корень."""
+    return sqrt(number)
+
+
+def calc(your_number: float) -> str:
+    """Выводит значение квадратного корня из заданного числа > 0 или 0.0."""
+    if your_number <= 0:
+        return '0.0'
+    root = calculate_square_root(your_number)
+    return (f'Мы вычислили квадратный корень из введённого вами числа. '
+            f'Это будет: {root}')
+
+
+print('Добро пожаловать в самую лучшую программу для вычисления '
+      'квадратного корня из заданного числа')
+result = calc(25.5)
+print(result)
+
+
+quit()
 
 # Тестовые данные.
 TEST_DATA: list[tuple[int, str, bool]] = [
