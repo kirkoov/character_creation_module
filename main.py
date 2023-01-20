519da649bc5d6355e0c91790f51b0ae262bc0290
Кирилл Кувшинов <k.koovshinov@gmail.com>
2023-01-20 09:10:14 +0300

    Finalize the PEP8 and mypy task and upload.

--- main.py
+++ main.py
@@ -1,61 +1,66 @@
 from random import randint
 
-
-DAMAGE_LINE = "нанёс урон противнику, равный"
-SPEC_SKILL_LINE = "применил специальное умение"
-
-
-ENTER_CMDS_LINE = ''.join([
+DAMAGE_LINE: str = 'нанёс урон противнику, равный'
+SPEC_SKILL_LINE: str = 'применил специальное умение'
+ENTER_CMDS_LINE: str = ''.join([
     'Введи одну из команд: attack — чтобы атаковать противника, ',
     'defence — чтобы блокировать атаку противника или special — чтобы',
-    'использовать свою суперсилу.'
+    ' использовать свою суперсилу.'
 ])
-
-
-ENTER_HERO_NAME_LINE = ''.join([
+ENTER_HERO_NAME_LINE: str = ''.join([
     'Введи название персонажа, за которого хочешь играть: Воитель — warrior, ',
     'Маг — mage, Лекарь — healer: '
 ])
-WARRIOR_LINE = ''.join(['Воитель — дерзкий воин ближнего боя. Сильный, ',
-                        'выносливый и отважный.'])
-MAGE_LINE = ''.join(['Маг — находчивый воин дальнего боя. Обладает высоким ',
-                     'интеллектом.'])
-HEALER_LINE = ''.join(['Лекарь — могущественный заклинатель. Черпает силы из ',
-                       'природы, веры и духов.'])
-APPR_CHOICE_LINE = ''.join([
+WARRIOR_LINE: str = ''.join(['Воитель — дерзкий воин ближнего боя. Сильный, ',
+                             'выносливый и отважный.'])
+MAGE_LINE: str = ''.join(['Маг — находчивый воин дальнего боя. Обладает ',
+                          'высоким интеллектом.'])
+HEALER_LINE: str = ''.join(['Лекарь — могущественный заклинатель. Черпает ',
+                            'силы из природы, веры и духов.'])
+APPR_CHOICE_LINE: str = ''.join([
     'Нажми (Y), чтобы подтвердить выбор, или любую другую кнопку, чтобы ',
     'выбрать другого персонажа '
 ])
 
 
-def attack(char_name, char_class):
+def attack(char_name: str, char_class: str) -> str:
+    """Docstring goes here."""
+    res: str = ''
     if char_class == 'warrior':
-        return (f'{char_name} {DAMAGE_LINE} {5 + randint(3, 5)}')
+        res = f'{char_name} {DAMAGE_LINE} {5 + randint(3, 5)}'
     if char_class == 'mage':
-        return (f'{char_name} {DAMAGE_LINE} {5 + randint(5, 10)}')
+        res = f'{char_name} {DAMAGE_LINE} {5 + randint(5, 10)}'
     if char_class == 'healer':
-        return (f'{char_name} {DAMAGE_LINE} {5 + randint(-3, -1)}')
+        res = f'{char_name} {DAMAGE_LINE} {5 + randint(-3, -1)}'
+    return res
 
 
-def defence(char_name, char_class):
+def defence(char_name: str, char_class: str) -> str:
+    """Docstring goes here."""
+    res: str = ''
     if char_class == 'warrior':
-        return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
+        res = f'{char_name} блокировал {10 + randint(5, 10)} урона'
     if char_class == 'mage':
-        return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
+        res = f'{char_name} блокировал {10 + randint(-2, 2)} урона'
     if char_class == 'healer':
-        return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
+        res = f'{char_name} блокировал {10 + randint(2, 5)} урона'
+    return res
 
 
-def special(char_name, char_class):
+def special(char_name: str, char_class: str) -> str:
+    """Docstring goes here."""
+    res: str = ''
     if char_class == 'warrior':
-        return (f'{char_name} {SPEC_SKILL_LINE} «Выносливость {80 + 25}»')
+        res = f'{char_name} {SPEC_SKILL_LINE} «Выносливость {80 + 25}»'
     if char_class == 'mage':
-        return (f'{char_name} {SPEC_SKILL_LINE} «Атака {5 + 40}»')
+        res = f'{char_name} {SPEC_SKILL_LINE} «Атака {5 + 40}»'
     if char_class == 'healer':
-        return (f'{char_name} {SPEC_SKILL_LINE} «Защита {10 + 30}»')
+        res = f'{char_name} {SPEC_SKILL_LINE} «Защита {10 + 30}»'
+    return res
 
 
-def start_training(char_name, char_class):
+def start_training(char_name: str, char_class: str) -> str:
+    """Docstring goes here."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -65,7 +70,7 @@ def start_training(char_name, char_class):
     print('Потренируйся управлять своими навыками.')
     print(ENTER_CMDS_LINE)
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd = None
+    cmd: str = ''
     while cmd != 'skip':
         cmd = input('Введи команду: ')
         if cmd == 'attack':
@@ -77,9 +82,10 @@ def start_training(char_name, char_class):
     return 'Тренировка окончена.'
 
 
-def choice_char_class():
-    approve_choice = None
-    char_class = None
+def choice_char_class() -> str:
+    """Docstring goes here."""
+    approve_choice: str = ''
+    char_class: str = ''
     while approve_choice != 'y':
         char_class = input(ENTER_HERO_NAME_LINE)
         if char_class == 'warrior':
@@ -92,16 +98,17 @@ def choice_char_class():
     return char_class
 
 
-def main():
+def main() -> None:
+    """Docstring goes here."""
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
-    print(start_training(char_name, char_class))
+    char_class: str = choice_char_class()
+    start_training(char_name, char_class)
 
 
 if __name__ == '__main__':
