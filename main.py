e2ddb0327c04ce7b67801d3b7dea8aef955e441e
Кирилл Кувшинов <k.koovshinov@gmail.com>
2023-01-25 07:51:42 +0300

    Add the remaining elements from the old main.py before the Ya check.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 The same character creation module, yet with classes, to carry on.
 """
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
+
 
 # Global string literals
 DEFAULT_ATTACK = 5
@@ -186,9 +188,20 @@ def start_training(character) -> str:
     return 'Тренировка окончена.'
 
 
-cat = choice_char_class('123')
-start_training(cat)
-
-# warrior = Warrior('Кодослав')
-# print(warrior)
-# print(warrior.attack())
+if __name__ == '__main__':
+    """Start the app, run the screensaver, game starts on pressing 'X'.
+    Args: None.
+    Return: None.
+    """
+    run_screensaver()
+    print('Приветствую тебя, искатель приключений!')
+    print('Прежде чем начать игру...')
+    char_name: str = input('...назови себя: ')
+    print(f'Здравствуй, {char_name}! '
+          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+    print('Ты можешь выбрать один из трёх путей силы:')
+    print('Воитель, Маг, Лекарь')
+    char_category = choice_char_class(char_name)
+    start_training(char_category)
+    # char_class: str = choice_char_class()
+    # start_training(char_name, char_class)
